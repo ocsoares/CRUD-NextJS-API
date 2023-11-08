2a409fdba5e0137267f80a05f293ad5b8d2785bd
ocsoares <casoares016@gmail.com>
2023-11-08 16:51:30 -0300

    chore: remove useless routes & files

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,5 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { CheckIfUserJwtExpiredService } from './use-cases/check-if-user-jwt-expired/check-if-user-jwt-expired.service';
-import { CheckIfUserJwtExpiredController } from './use-cases/check-if-user-jwt-expired/check-if-user-jwt-expired.controller';
-import { GenerateUserGoogleTokenController } from './use-cases/generate-user-google-token/generate-user-google-token.controller';
-import { GenerateUserGoogleTokenService } from './use-cases/generate-user-google-token/generate-user-google-token.service';
 import { GenerateUserGithubTokenService } from './use-cases/generate-user-github-token/generate-user-github-token.service';
 import { GenerateUserGithubTokenController } from './use-cases/generate-user-github-token/generate-user-github-token.controller';
 import { HttpModule } from '@nestjs/axios';
@@ -19,16 +15,8 @@ import { rateLimiterMiddleware } from '../auth/middlewares/rate-limiter.middlewa
         }),
         HttpModule,
     ],
-    controllers: [
-        CheckIfUserJwtExpiredController,
-        GenerateUserGoogleTokenController,
-        GenerateUserGithubTokenController,
-    ],
-    providers: [
-        CheckIfUserJwtExpiredService,
-        GenerateUserGoogleTokenService,
-        GenerateUserGithubTokenService,
-    ],
+    controllers: [GenerateUserGithubTokenController],
+    providers: [GenerateUserGithubTokenService],
 })
 export class UserModule implements NestModule {
     // I used this because NestJS Throttler Module doesn't work !!!
